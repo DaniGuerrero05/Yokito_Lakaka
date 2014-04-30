7f91d00b9cf227bb1a16e59ba62bcea0a61fb4ef
DaniGuerrero05 <dani.guerrero05@gmail.com>
2014-04-30 17:18:33 +0200

    Comentarios

--- pb.c
+++ pb.c
@@ -32,29 +32,31 @@ int calculate_result(int white_balls[5], int power_ball)
 	  		return -1;
 		}
 		return -1;
-	}
-	qsort(white_balls, 5, sizeof(int), my_sort_func);
-	return 0;
 }
-
+}
+// lottery ball numbers are always shown sorted
+qsort(white_balls, 5, sizeof(int), my_sort_func);
+return 0;
+}
 
 int main(int argc, char** argv)
 {
 int balls[6];
 int count_balls = 0;
-int favorite = 0;
+int favorite = 0; // this should be a bool
 for (int i=1; i<argc; i++)
 {
-const char* arg = argv[i];
-if ('-' == arg[0])
-{
-if (0 == strcmp(arg, "-favorite"))
-{
-favorite = 1;
+goto usage_error;
 }
-else
+// the power ball is always the last one given
+int power_ball = balls[5];
+int result = calculate_result(balls, power_ball);
+// calculate result can return -1 if the ball numbers
+// are out of range
+if (result < 0)
 {
 goto usage_error;
+
 }
 }
 else
